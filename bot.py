376b413b65513b438a8f7c992c83e50f90a1deee
chris <christsironiss@gmail.com>
2021-12-11 20:34:48 +0200

    heroku ready

--- bot.py
+++ bot.py
@@ -18,6 +18,7 @@ chrome_options.add_argument("--start-maximized")
 chrome_options.add_argument("--window-size=1920x1080") 
 chrome_options.add_argument('disable-blink-features=AutomationControlled')
 chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36')
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 browser = webdriver.Chrome(options=chrome_options)
 
 browser.get(('https://vod.antenna.gr'))
