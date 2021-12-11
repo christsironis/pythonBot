0118c160e23c5c34ef5e409b420b8dc576c6e181
chris <christsironiss@gmail.com>
2021-12-11 19:08:24 +0200

    heroku ready

--- bot.py
+++ bot.py
@@ -16,16 +16,16 @@ username= "christsironiss@gmail.com"
 password= "abcdefghik"
 
 chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument('--single-process')
-# chrome_options.add_argument('--ignore-certificate-errors')
+chrome_options.add_argument('--single-process')
+chrome_options.add_argument('--ignore-certificate-errors')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
-# chrome_options.add_argument("--disable-dev-shm-usage")
-# chrome_options.add_argument("--start-maximized")
-# chrome_options.add_argument("--window-size=1920x1080") 
-# chrome_options.add_argument('disable-blink-features=AutomationControlled')
-# chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36')
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_argument("--window-size=1920x1080") 
+chrome_options.add_argument('disable-blink-features=AutomationControlled')
+chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36')
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
