dd52e2c289657ca44841f1dc308f36f23a45fb35
chris <christsironiss@gmail.com>
2021-12-11 20:17:30 +0200

    heroku ready

--- bot.py
+++ bot.py
@@ -10,16 +10,17 @@ username= "christsironiss@gmail.com"
 password= "abcdefghik"
 
 chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument('--single-process')
-# chrome_options.add_argument('--ignore-certificate-errors')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
+#  The first one will make it so the "navigator.webdriver=true" variable in javascript doesn't show. Sites can access that variable to check if your using automation and block you or make you solve a captcha.
+chrome_options.add_argument('disable-blink-features=AutomationControlled')
+chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36')
+# chrome_options.add_argument('--single-process')
+# chrome_options.add_argument('--ignore-certificate-errors')
 # chrome_options.add_argument("--disable-dev-shm-usage")
 # chrome_options.add_argument("--start-maximized")
 # chrome_options.add_argument("--window-size=1920x1080") 
-# chrome_options.add_argument('disable-blink-features=AutomationControlled')
-chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36')
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
@@ -34,6 +35,7 @@ browser.switch_to.window(browser.window_handles[-1])
 
 print(browser.window_handles)
 print(browser.current_url)
+
 # pop window interaction
 userInput = browser.find_element(By.ID,'loginId')
 userInput.send_keys(username)
@@ -41,12 +43,14 @@ passInput = browser.find_element(By.ID,'password')
 passInput.send_keys(password)
 sundesh = browser.find_element(By.XPATH,'//div[@class="form-row"]//button')
 sundesh.click()
+
 # finds the request that has the password
 browser.wait_for_request("https://api.antenna.gr/v100/api/auth.class.api.php/logon/354",10)
 for request in browser.requests:
 	if request.url == "https://api.antenna.gr/v100/api/auth.class.api.php/logon/354":
 			token=url.urlencode(request.params)
 			print(token)
+
 # sends the password to the redis server   
 red = redis.Redis(host='redis-13661.c233.eu-west-1-1.ec2.cloud.redislabs.com', port='13661', 
                 password='CyPk7oc145cDyTnKvVfVVrDF3Ic0NZa5')
