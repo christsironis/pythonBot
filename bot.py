a9ccc018144bcc5c64c4a6240a5838ec479861bd
chris <christsironiss@gmail.com>
2022-01-07 16:46:25 +0200

    remote session

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ from selenium.webdriver.support import expected_conditions as EC
 import urllib.parse as url
 import requests
 import smtplib
-import browser
 import redis
 import sys
 import re
@@ -95,6 +94,23 @@ def LogIn():
             session_id.strip()
             print(url)
             print(session_id)
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--disable-gpu')
+            chrome_options.add_argument('--no-sandbox')
+            #  The first one will make it so the "navigator.webdriver=true" variable in javascript doesn't show. Sites can access that variable to check if your using automation and block you or make you solve a captcha.
+            chrome_options.add_argument(
+                'disable-blink-features=AutomationControlled')
+            chrome_options.add_argument(
+                'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36')
+            chrome_options.add_argument('--single-process')
+            chrome_options.add_argument('--ignore-certificate-errors')
+            chrome_options.add_argument("--disable-dev-shm-usage")
+            chrome_options.add_argument("--start-maximized")
+            chrome_options.add_argument("--window-size=1920x1080")
+            chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+            browser = webdriver.Remote(command_executor=url, chrome_options=chrome_options)
+            browser.session_id = session_id
         else:
             browser = CreateBrowser()
             writeSession(browser)
