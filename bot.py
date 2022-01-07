d78e940fcb8978489ce14901cc8d5300b8e7593b
chris <christsironiss@gmail.com>
2022-01-07 14:56:34 +0200

    reusing same webdriver, without quiting

--- bot.py
+++ bot.py
@@ -1,16 +1,14 @@
 from datetime import date
-from seleniumwire import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from browser import browser 
 import urllib.parse as url
 import requests
 import smtplib
 import redis
 import sys
-import os
 import re
-
 
 def SendEmail(error):
     try:
@@ -42,24 +40,6 @@ def LogIn():
         # username = "tsiochris0001@yahoo.gr"
         username = "christsironiss@gmail.com"
         password = "abcdefghik"
-
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--disable-gpu')
-        chrome_options.add_argument('--no-sandbox')
-        #  The first one will make it so the "navigator.webdriver=true" variable in javascript doesn't show. Sites can access that variable to check if your using automation and block you or make you solve a captcha.
-        chrome_options.add_argument(
-            'disable-blink-features=AutomationControlled')
-        chrome_options.add_argument(
-            'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36')
-        chrome_options.add_argument('--single-process')
-        chrome_options.add_argument('--ignore-certificate-errors')
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--start-maximized")
-        chrome_options.add_argument("--window-size=1920x1080")
-        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        browser = webdriver.Chrome(executable_path=os.environ.get(
-            "CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
         browser.get(('https://vod.antenna.gr'))
 
@@ -96,7 +76,7 @@ def LogIn():
         red.set('old_password', old)
         red.set('password', token)
 
-        browser.quit()
+        # browser.quit()
         return token
 
     except:
